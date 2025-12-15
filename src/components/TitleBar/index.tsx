e90e0487c5b306982b669e6a57f501f2608ac6c2
杨再涛 <1772081518@qq.com>
2025-12-15 20:41:03 +0800

    动态更改全屏切换

--- src/components/TitleBar/index.tsx
+++ src/components/TitleBar/index.tsx
@@ -1,28 +1,31 @@
 import { CloseButton, Label } from '@heroui/react';
 import { Icon } from '@iconify/react';
-import { app } from '@tauri-apps/api'
 import { Window } from '@tauri-apps/api/window';
 import './index.css';
+import { useState } from 'react';
 
 
 const current = Window.getCurrent()
-
-function onBtnDown() {
-    alert(app.getVersion())
-}
-
 const APP_Name = import.meta.env['VITE_NAME']
 export default function TitleBar() {
+    const [full, setFull] = useState(false)
+
+    async function ToggleMaxmize() {
+        await current.toggleMaximize()
+        await current.isMaximized().then(setFull)
+    }
+
     return <div className="head shadow-md">
-        <Label className="float-left text-surface">{APP_Name}</Label>
+        <Label className="float-left text-surface">{APP_Name}{String(full)}</Label>
         <div className="title_bar">
-            <CloseButton onClick={() => current.minimize()}>
+            <CloseButton onClick={() => current.minimize()} className="cursor-pointer">
                 <Icon icon="material-symbols:check-indeterminate-small-rounded" />
             </CloseButton>
-            <CloseButton onClick={() => current.toggleMaximize()}>
-                <Icon icon="material-symbols:fullscreen" />
+            <CloseButton onClick={ToggleMaxmize} className="cursor-pointer">
+                {/* <Icon icon="solar:full-screen-bold" width="24" height="24" /> */}
+                <Icon icon={`material-symbols:${full ? 'fullscreen-exit' : 'fullscreen'}`} width="24" height="24" />
             </CloseButton>
-            <CloseButton onClick={() => current.close()} className="text-red-700">
+            <CloseButton onClick={() => current.close()} className="text-red-700 cursor-pointer">
                 <Icon icon="material-symbols:close-rounded" />
             </CloseButton>
         </div>
