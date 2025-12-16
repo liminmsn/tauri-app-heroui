c42da76625035e19ad908b04afeb6ceb9e140ef3
杨再涛 <1772081518@qq.com>
2025-12-16 14:31:31 +0800

    优化代码

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import TitleBar from "./components/BZH_TitleBar";
 
 function App() {
   return (
-    <main className="main bg-foreground text-surface">
+    <main className="h-full bg-foreground text-surface">
       <TitleBar />
-      <div className="h-full p-2">213</div>
+      <div className="p-1">213</div>
     </main>
   );
 }
