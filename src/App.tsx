1d1908d78502a0b1aede2c1e9f9381e2c0518400
杨再涛 <1772081518@qq.com>
2025-12-17 14:04:01 +0800

    全屏判断

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./design/App.css";
 import TitleBar from "./components/BZH_TitleBar";
+import { useBZHContext } from "./context";
 
 function App() {
+  const [data] = useBZHContext()
+
   return (
-    <main className="h-full bg-foreground text-surface">
+    <main className={`h-full bg-foreground text-surface ${data.full || 'rounded-md'}`}>
       <TitleBar />
       <div className="p-1">213</div>
     </main>
